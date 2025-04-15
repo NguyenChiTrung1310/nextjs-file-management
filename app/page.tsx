97fabdc3dc5de2f097b8fb74c2cb524e90db05aa
Trung Nguyen <chitrung.nguyen@vietlink.jp>
2025-04-15 11:56:53 +0700

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className='flex-center'>
+    <div className='flex-center h-screen'>
       <h1 className='text-brand'>Home page</h1>
     </div>
   )
